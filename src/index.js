c9c994c53ff86bda0743e4a4bfeb3db532c7a8bc
Ruulul <vamjunior01@gmail.com>
2023-01-03 16:24:55 -0300

    adding custom bar and fill

--- src/index.js
+++ src/index.js
@@ -8,18 +8,83 @@ function RangeSlider () {
     const input = e('input')
     input.type = 'range'
 
+    const bar = utils.eX('bar')
+    const ruler = utils.eX('ruler')
+    const fill = utils.eX('fill')
+    bar.append(ruler, fill)
+
     const style = e('style')
     style.textContent = getTheme()
 
-    shadow.append(style, input)
+    shadow.append(style, input, bar)
 
     return el
 }
 
+const handlers = {
+
+}
+const utils = {
+    eX(x, el = 'div') {
+        const elm = e(el)
+        elm.classList.add(x)
+        return elm
+    }
+}
+const actions = {
+
+}
+
 function getTheme () {
     return `
-        input {
+        * {
+            box-sizing: border-box;
             width: 100%;
+            height: 2em;
+            position: relative;
+            --transparent: hsla(0, 0%, 0%, 0);
+            --grey: hsl(0, 0%, 80%);
         }
-    `
+        input {
+            position: absolute;
+            top: 0.5em;
+            left: 0;
+            width: 100%;
+            height: 1em;
+            -webkit-appearance: none;
+            z-index: 2; 
+            background-color: var(--transparent);
+        }
+        .bar {
+            position: absolute;
+            top: 0.5em;
+            left: 0;
+            height: 1em;
+            width: 100%;
+            border-radius: 1em;
+            background-color: var(--grey);
+            display: flex;
+            flex-flow: column;
+            justify-content: center;
+        }
+        .ruler {
+            position: absolute;
+            height: 0.5em;
+            width: 100%;
+            transform: scale(-1, 1);
+            background-size: 3em 1em;
+            background-color: repeating-linear-gradient(to right,
+                var(--grey) 0,
+                var(--grey) 2em,
+                white 2em,
+                white 3em
+            );
+        }
+        .fill {
+            position: absolute;
+            height: 100%;
+            width: 30%;
+            background-color: var(--grey);
+        }
+   `
 }
